b5a1440397d7cf9b3de3acd41833ee0de97fe811
Aline de Souza Lima Abreu <alineslima17@gmail.com>
2020-04-15 23:43:13 -0300

    add project

---
+++ src/main/java/br/com/codenation/desafioexe/DesafioApplication.java
@@ -0,0 +1,31 @@
+package br.com.codenation.desafioexe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DesafioApplication {
+
+	public static List<Integer> fibonacci() {
+		List<Integer> res = new ArrayList<>();
+		res.add(0, 0);
+		res.add(1, 1);
+		int i = 2;
+		int atual = 0;
+		while(atual < 350){
+			atual = (res.get(i-2) + res.get(i-1));
+			res.add(i,atual);
+			++i;
+		}
+		System.out.println(res);
+		return res;
+	}
+
+	public static Boolean isFibonacci(Integer a) {
+		List<Integer> res = fibonacci();
+		if(res.contains(a)){
+			return true;
+		}
+		return false;
+	}
+
+}
